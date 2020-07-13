0545532b08637838c6b8216f3496380c5ac77e31
shuvam1 <shuvam@openhouse.study>
2020-07-13 18:26:52 +0530

    Adding Code to fetch Tab List

--- app/popup.js
+++ app/popup.js
@@ -1,4 +1,18 @@
+function byAlphabeticalURLOrder(tab1, tab2) {
+    if (tab1.url < tab2.url) {
+      return -1;
+    } else if (tab1.url > tab2.url) {
+      return 1;
+    }
+    return 0;
+  }
+
+
+
 chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+
+    tabs.sort(byAlphabeticalURLOrder);
+    
     document.write(`<h3>The tabs you're on are:</h3>`);
     document.write('<ul>');
     for (let i = 0; i < tabs.length; i++) {
