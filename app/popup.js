a29d0e0e6114087492bbfa533216616e49eb5a13
shuvam1 <shuvam@openhouse.study>
2020-07-15 23:16:17 +0530

    Adding Code to fetch Tab List

--- app/popup.js
+++ app/popup.js
@@ -12,11 +12,33 @@ function byAlphabeticalURLOrder(tab1, tab2) {
 chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
 
     tabs.sort(byAlphabeticalURLOrder);
-    
-    document.write(`<h3>The tabs you're on are:</h3>`);
-    document.write('<ul>');
-    for (let i = 0; i < tabs.length; i++) {
-      document.write(`<li>${tabs[i].url}</li>`);
-    }
-    document.write('</ul>');
+    // chrome.tabs.move(tabs[0].id, {index: 0});
+
+    // for (let i = 0; i < tabs.length; i++) {
+    //   chrome.tabs.move(tabs[i].id, {index: i});
+    // }
+
+    //var newURL = "http://stackoverflow.com/";
+    //chrome.tabs.create({ url: newURL });
+
+    chrome.storage.sync.set({'data' : [{'foo': 'hello', 'bar': 'hi'}, {'foo': 'it', 'bar': 'works'}]}, function() {
+        console.log('Settings saved');
+      });
+
+     // Read it using the storage API
+        chrome.storage.sync.get(['data1'], function(items) {
+            try {
+                document.write(items.data[1].bar);
+            }
+            catch(e) {
+                document.write(e);
+            }
+          });    
+     
+    // document.write(`<h3>The tabs you're on are:</h3>`);
+    // document.write('<ul>');
+    // for (let i = 0; i < tabs.length; i++) {
+    //   document.write(`<li>${tabs[i].title}</li>`);
+    // }
+    // document.write('</ul>');
   });
