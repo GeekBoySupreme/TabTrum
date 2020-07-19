46bfcf3718d9c8412727bfc15b5ae23e0eec0993
geekboysupreme <shuvam.manna@gmail.com>
2020-07-19 14:57:39 +0530

    Fixing Memory Access

--- app/popup.js
+++ app/popup.js
@@ -1,44 +1,141 @@
-function byAlphabeticalURLOrder(tab1, tab2) {
-    if (tab1.url < tab2.url) {
-      return -1;
-    } else if (tab1.url > tab2.url) {
-      return 1;
+// function byAlphabeticalURLOrder(tab1, tab2) {
+//     if (tab1.url < tab2.url) {
+//       return -1;
+//     } else if (tab1.url > tab2.url) {
+//       return 1;
+//     }
+//     return 0;
+//   }
+
+
+
+// chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+
+//     tabs.sort(byAlphabeticalURLOrder);
+//     // chrome.tabs.move(tabs[0].id, {index: 0});
+
+//     // for (let i = 0; i < tabs.length; i++) {
+//     //   chrome.tabs.move(tabs[i].id, {index: i});
+//     // }
+
+//     //var newURL = "http://stackoverflow.com/";
+//     //chrome.tabs.create({ url: newURL });
+
+//     chrome.storage.sync.set({'data' : [{'foo': 'hello', 'bar': 'hi'}, {'foo': 'it', 'bar': 'works'}]}, function() {
+//         console.log('Settings saved');
+//       });
+
+//      // Read it using the storage API
+//         chrome.storage.sync.get(['data'], function(items) {
+//             try {
+//                 justdata = {'foo': 'it', 'bar': 'blah'};
+//                 items.data.push(justdata);
+//                 //document.write(items.data[2].bar);
+//             }
+//             catch(e) {
+//                 document.write(e);
+//             }
+//           });    
+     
+//     // document.write(`<h3>The tabs you're on are:</h3>`);
+//     // document.write('<ul>');
+//     // for (let i = 0; i < tabs.length; i++) {
+//     //   document.write(`<li>${tabs[i].title}</li>`);
+//     // }
+//     // document.write('</ul>');
+//   });
+
+loadData();
+
+  var button = document.getElementById('snapshot_button');
+  button.onclick = function() {
+      try {
+        var tab_data = getData();
+        if(tab_data) {
+          chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+            var new_snapshot = {
+              'title' : 'Snapshot' + tab_data.length,
+              'id' : 'id_' + tab_data.length,
+              'tabs' : tabs
+            }
+  
+            tab_data.push(new_snapshot);
+            setData(tab_data); 
+          });
+        }
+        else {
+          throw new Error('Exception message');
+        }
+      }
+      catch(e) {
+        var init_data = [];
+        chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+          var new_snapshot = {
+            'title' : 'Snapshot ' + init_data.length,
+            'id' : 'id_' + init_data.length,
+            'tabs' : tabs
+          }
+
+          init_data.push(new_snapshot);
+          setData(init_data); 
+        });
+      }
+
+      loadData();
+  };
+
+
+
+
+  function loadData() {
+
+    console.log("Hello");
+    var render_home = '';
+    // var render_data = getData();
+    // console.log(render_data);
+
+    try {
+      chrome.storage.sync.get(['data'], function(items) {
+        render_data = items.data;
+        
+        for(var i=0; i < render_data.length; i++) {
+          render_home += '<div class="snapshot_tab" id="snapshot_tab_'+ render_data[i].id +'"> \
+                          <h5 class="snapshot_title">'+ render_data[i].title +'</h5> \
+                          <button class="snapshot_launch" id="snapshot_launch_'+ render_data[i].id +'">Launch Tabs</button> \
+                          </div>'
+        }
+
+        console.log(render_home);
+        document.getElementById('snapshot_list').innerHTML = render_home;
+      });
     }
-    return 0;
+    catch(e) {
+      console.log("Error : "+e);
+
+      var data_initialize = [];
+      setData(data_initialize);
+    } 
   }
 
 
+  function getData() {
+    var stored_data = '';
 
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-
-    tabs.sort(byAlphabeticalURLOrder);
-    // chrome.tabs.move(tabs[0].id, {index: 0});
-
-    // for (let i = 0; i < tabs.length; i++) {
-    //   chrome.tabs.move(tabs[i].id, {index: i});
-    // }
-
-    //var newURL = "http://stackoverflow.com/";
-    //chrome.tabs.create({ url: newURL });
-
-    chrome.storage.sync.set({'data' : [{'foo': 'hello', 'bar': 'hi'}, {'foo': 'it', 'bar': 'works'}]}, function() {
-        console.log('Settings saved');
+    try {
+      chrome.storage.sync.get(['data'], function(items) {
+        stored_data = items.data;
+        console.log(items.data);
       });
+    }
+    catch(e) {
+      console.log("Error : "+e);
+    }
 
-     // Read it using the storage API
-        chrome.storage.sync.get(['data1'], function(items) {
-            try {
-                document.write(items.data[1].bar);
-            }
-            catch(e) {
-                document.write(e);
-            }
-          });    
-     
-    // document.write(`<h3>The tabs you're on are:</h3>`);
-    // document.write('<ul>');
-    // for (let i = 0; i < tabs.length; i++) {
-    //   document.write(`<li>${tabs[i].title}</li>`);
-    // }
-    // document.write('</ul>');
-  });
+    return stored_data;
+  }
+
+  function setData(data_update) {
+    chrome.storage.sync.set({'data' : data_update}, function() {
+      console.log('Settings saved');
+    });
+  }
